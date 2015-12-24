ae32d8891e75f10cdc63cfe40765790e3fb710a2
Matthew Clarkson <mpclarkson@gmail.com>
2015-12-24 14:07:48 +1000

    Initial Commit

---
+++ HISwiftExtensions/HISwiftExtensions.h
@@ -0,0 +1,19 @@
+//
+//  HISwiftExtensions.h
+//  HISwiftExtensions
+//
+//  Created by Matthew on 24/12/2015.
+//  Copyright © 2015 Hilenium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HISwiftExtensions.
+FOUNDATION_EXPORT double HISwiftExtensionsVersionNumber;
+
+//! Project version string for HISwiftExtensions.
+FOUNDATION_EXPORT const unsigned char HISwiftExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HISwiftExtensions/PublicHeader.h>
+
+
