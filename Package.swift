925c144365f07f3ffa76866b4a777e2da4e79fb6
Matthew Clarkson <mpclarkson@gmail.com>
2015-12-28 10:17:05 +1000

    package text missing

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  HISwiftExtensions
+//
+//  Created by Matthew on 28/12/2015.
+//  Copyright © 2015 Hilenium. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "HISwiftExtensions"
+)
+
