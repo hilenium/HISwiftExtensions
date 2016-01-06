90cd8215b1789ecf22d08cd8eea5b005cfefd9e8
Matthew Clarkson <mpclarkson@gmail.com>
2016-01-06 12:34:21 +1000

    docs script create subtree 2

--- Resources/scripts/docs.sh
+++ Resources/scripts/docs.sh
@@ -6,12 +6,11 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
     gem install jazzy
     echo -e "Generating documentation \n"
     jazzy --swift-version 2.1 --source-directory ./ --output .Resources/docs --podspec ./HISwiftExtensions.podspec
-    echo -e "Creating subtree \n"
-    git add Resources/docs
     echo -e "Adding new documentation \n"
     git add -A
     git commit -m "Updated docs from travis build $TRAVIS_BUILD_NUMBER"
-    # git push --force --quiet "https://${GH_TOKEN}@github.com/hilenium/HISwiftExtensions" master:gh-pages > /dev/null 2>&1
+    echo -e "Creating docs subtree \n"
+    git add Resources/docs
     git subtree push --prefix Resources/docs origin gh-pages
     echo -e "Published latest docs.\n"
 fi
