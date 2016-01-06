4a4e3fa6bff9c15359b64bdf63e7cc6ea787e130
travis-ci <travis@travis-ci.org>
2016-01-06 14:06:22 +1000

    travis docs should be working

--- Resources/scripts/docs.sh
+++ Resources/scripts/docs.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     echo -e "*** Generating docs ***\n"
     echo -e "*** Installing Jazzy ***\n"
     gem install jazzy
-    echo -e "*** Generating documentation ***\n"
-    jazzy --swift-version 2.1.1 --source-directory ./ --output .Resources/docs --podspec ./HISwiftExtensions.podspec
+    echo -e "*** Creating documentation folder ***\n"
+    rm -rf out || exit 0;
+    mkdir out;
+    cd out
     echo -e "*** Setting up git ***\n"
+    git init
     git config user.email "travis@travis-ci.org"
     git config user.name "travis-ci"
+    echo -e "*** Generating documentation ***\n"
+    jazzy --swift-version 2.1 --source-directory ../ --output . --podspec ../HISwiftExtensions.podspec
     echo -e "*** Adding docs to git ***\n"
-    git add -A
+    git add .
     git commit -m "Updated docs from travis build $TRAVIS_BUILD_NUMBER"
-    echo -e "*** Creating docs subtree \n ***"
-    git add Resources/docs
-    git subtree push --prefix Resources/docs "https://${GH_TOKEN}@github.com/hilenium/HISwiftExtensions" gh-pages
+    git push --force --quiet "https://${GH_TOKEN}@github.com/hilenium/HISwiftExtensions" master:gh-pages > /dev/null 2>&1
     echo -e "*** Published latest docs ***\n"
-# fi
+fi
