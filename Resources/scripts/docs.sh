d401543c61334d1893e4312b8a7f12c38a00e3b0
Matthew Clarkson <mpclarkson@gmail.com>
2016-01-06 11:55:50 +1000

    documentation script

--- Resources/scripts/docs.sh
+++ Resources/scripts/docs.sh
@@ -11,19 +11,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
     #
     # echo -e "Moving into gh-pages clone \n"
     # pushd gh-pages
-
-    echo -e "Creating gh-pages repo \n"
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "travis-ci"
-    git init
+    #
+    # echo -e "Creating gh-pages repo \n"
+    # git config --global user.email "travis@travis-ci.org"
+    # git config --global user.name "travis-ci"
 
     echo -e "Generating documentation \n"
-    jazzy --swift-version 2.1 --source-directory ../ --output .Resources/documentation --podspec ../HISwiftExtensions.podspec
+    jazzy --swift-version 2.1 --source-directory ../ --output .Resources/docs --podspec ../HISwiftExtensions.podspec
 
     echo -e "Adding new documentation \n"
     git add -A
     git commit -m "Updated docs from travis build $TRAVIS_BUILD_NUMBER"
-    git push --force --quiet "https://${GH_TOKEN}@github.com/hilenium/HISwiftExtensions" master:gh-pages > /dev/null 2>&1
+    # git push --force --quiet "https://${GH_TOKEN}@github.com/hilenium/HISwiftExtensions" master:gh-pages > /dev/null 2>&1
+    git subtree push --prefix Resources/docs origin gh-pages
     echo -e "Published latest docs.\n"
 
     echo -e "Finished..."
