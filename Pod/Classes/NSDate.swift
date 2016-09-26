1178bd9f4b895262e3a9b0376f2be286c4d2ac5d
Matthew Clarkson <matt@hilenium.com>
2016-09-26 10:17:51 +1000

    updated time ago

--- Pod/Classes/NSDate.swift
+++ Pod/Classes/NSDate.swift
@@ -41,58 +41,83 @@ public extension Date {
         let earliest = (now as NSDate).earlierDate(self)
         let latest = (earliest == now) ? self : now
         
-        let components:DateComponents = (calendar as NSCalendar).components(unitFlags, from: earliest, to: latest, options: [])
+        let components: DateComponents = (calendar as NSCalendar).components(unitFlags, from: earliest, to: latest, options: [])
         
-        if (components.year! >= 2) {
-            return "\(components.year) years ago"
-        } else if (components.year! >= 1){
-            if (numericDates){
-                return "1 year ago"
-            } else {
-                return "Last year"
+        if let year = components.year {
+            if year >= 2 {
+                return "\(year) years ago"
             }
-        } else if (components.month! >= 2) {
-            return "\(components.month) months ago"
-        } else if (components.month! >= 1){
-            if (numericDates){
-                return "1 month ago"
-            } else {
-                return "Last month"
+            else if year >= 1 {
+                if numericDates {
+                    return "1 year ago"
+                } else {
+                    return "Last year"
+                }
             }
-        } else if (components.weekOfYear! >= 2) {
-            return "\(components.weekOfYear) weeks ago"
-        } else if (components.weekOfYear! >= 1){
-            if (numericDates){
-                return "1 week ago"
-            } else {
-                return "Last week"
+        }
+        else if let month = components.month {
+            if month >= 2 {
+                 return "\(month) months ago"
             }
-        } else if (components.day! >= 2) {
-            return "\(components.day) days ago"
-        } else if (components.day! >= 1){
-            if (numericDates){
-                return "1 day ago"
-            } else {
-                return "Yesterday"
+            else if month >= 1 {
+                if numericDates {
+                    return "1 month ago"
+                } else {
+                    return "Last month"
+                }
             }
-        } else if (components.hour! >= 2) {
-            return "\(components.hour) hours ago"
-        } else if (components.hour! >= 1){
-            if (numericDates){
-                return "1 hour ago"
-            } else {
-                return "An hour ago"
+        }
+        else if let week = components.weekOfYear {
+            if week >= 2 {
+                 return "\(week) weeks ago"
             }
-        } else if (components.minute! >= 2) {
-            return "\(components.minute) minutes ago"
-        } else if (components.minute! >= 1){
-            if (numericDates){
-                return "1 minute ago"
-            } else {
-                return "A minute ago"
+            else if week >= 1  {
+                if numericDates {
+                    return "1 week ago"
+                } else {
+                    return "Last week"
+                }
             }
-        } else if (components.second! >= 3) {
-            return "\(components.second) seconds ago"
+        }
+        else if let day = components.day {
+            if day >= 2 {
+                 return "\(day) days ago"
+            }
+            else if day >= 1 {
+                
+                if numericDates {
+                    return "1 day ago"
+                } else {
+                    return "Yesterday"
+                }
+            }
+        
+        } else if let hour = components.hour {
+            if hour >= 2 {
+                 "\(hour) hours ago"
+            }
+            else if hour >= 1 {
+                if numericDates {
+                    return "1 hour ago"
+                } else {
+                    return "An hour ago"
+                }
+            }
+        }
+        
+        else if let minute = components.minute {
+            if minute >= 2 {
+                 return "\(minute) minutes ago"
+            }
+            else if minute >= 1 {
+                if numericDates {
+                    return "1 minute ago"
+                } else {
+                    return "A minute ago"
+                }
+            }
+        } else if let second = components.second, second >= 10 {
+            return "\(second) seconds ago"
         } else {
             return "Just now"
         }
